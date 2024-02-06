f8159bab0fe077819ae8e336b19606b8ab9fd434
Vicente Camarena <vicente.camarena@konicaminolta.es>
2024-02-06 17:27:46 +0100

    Version 0.1.0

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuinielaComunitaria
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuinielaComunitaria
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
