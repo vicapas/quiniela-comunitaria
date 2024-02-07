a0045f01a62a7106940cb88a6d7f6a3391d6c044
Vicente Camarena <vicente.camarena@konicaminolta.es>
2024-02-07 10:38:26 +0100

    Add SqlLite

---
+++ Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace QuinielaComunitaria.Models
+{
+    public class User
+    {
+        public string? Name { get; set; }
+    }
+}
---
+++ Services/Db.cs
@@ -0,0 +1,21 @@
+﻿using QuinielaComunitaria.Models;
+using SQLite;
+
+namespace QuinielaComunitaria.Services
+{
+    public class Db
+    {
+        const string DbFileName = "quinieladb.db3";
+        static string DbPath = Path.Combine(FileSystem.AppDataDirectory, DbFileName);
+        const SQLiteOpenFlags Flags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+        SQLiteAsyncConnection? Database;
+
+        public async Task Init()
+        {
+            if (Database is not null) return;
+            Database = new SQLiteAsyncConnection(DbPath, Flags);
+            var result = await Database.CreateTableAsync<User>();
+        }
+    }
+}
