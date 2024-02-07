35cb5f4a15b09ffd51711a1f61b24364c485f87c
Vicente Camarena <vicente.camarena@konicaminolta.es>
2024-02-07 11:58:12 +0100

    Add user controls

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using QuinielaComunitaria.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace QuinielaComunitaria.ViewModels
 {
     public class MainViewModel : BindableObject
     {
-        private ObservableCollection<string> userList = [];
-        public ObservableCollection<string> UserList
+        private ObservableCollection<User> userList = [];
+        public ObservableCollection<User> UserList
         {
             get => userList;
             set 
@@ -18,9 +20,32 @@ namespace QuinielaComunitaria.ViewModels
             }
         }
 
+        private string? addUserText = string.Empty;
+        public string? AddUserText
+        {
+            get => addUserText;
+            set
+            {
+                if (addUserText != value)
+                {
+                    addUserText = value;
+                    OnPropertyChanged(nameof(AddUserText));
+                }
+            }
+        }
+
+        public ICommand? AddUserCommand { get; set; }
+
         public MainViewModel()
         {
-            UserList = [ "User 1", " User 2", "User 3", "User 4" ];
+            AddUserCommand = new Command(AddUser);
+        }
+
+        private void AddUser()
+        {
+            if (string.IsNullOrEmpty(AddUserText)) return;
+            UserList.Add(new User {  Name = AddUserText });
+            AddUserText = string.Empty;
         }
     }
 }
